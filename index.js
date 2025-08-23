56d8f6344185a3db3f02179dd6e4ffebc9e73a0e
Azam-aa <azamp442@gmail.com>
2025-08-23 20:54:23 +0530

    Testing event

---
+++ index.js
@@ -0,0 +1,12 @@
+var nodb = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<nodb; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+    var buttonInnerHTML = this.innerHTML;
+
+    
+});
+}
+
